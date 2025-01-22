d469a75e11dfd4700087491cef7a21ca554a4534
daku008 <dakub2238@gmail.com>
2025-01-22 19:26:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 BOT_TOKEN = "7718352742:AAEtXzO3ziZ8oD5M1yXDOHj1yLHnXUHVCaM"
 ADMIN_IDS = ["1944182800"]
-GROUP_ID = -1002270145423  # Replace with your group ID
-GROUP_LINK = "https://t.me/DAKUFREEBOT"  # Replace with your group invite link
+GROUP_ID = -1002261319850  # Replace with your group ID
+GROUP_LINK = "https://t.me/+K00vpyRUtbE5ZGE1"  # Replace with your group invite link
 # Default number of threads for attacks
-DEFAULT_THREADS = 900  # Set the default number of threads to use for attacks
+DEFAULT_THREADS = 30  # Set the default number of threads to use for attacks
