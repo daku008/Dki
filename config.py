11ad52fbfe898861225a56eeeec427d84073ccfe
daku008 <dakub2238@gmail.com>
2025-01-22 19:11:18 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+BOT_TOKEN = "7718352742:AAEtXzO3ziZ8oD5M1yXDOHj1yLHnXUHVCaM"
+ADMIN_IDS = ["1944182800"]
+GROUP_ID = -1002270145423  # Replace with your group ID
+GROUP_LINK = "https://t.me/DAKUFREEBOT"  # Replace with your group invite link
+# Default number of threads for attacks
+DEFAULT_THREADS = 900  # Set the default number of threads to use for attacks
